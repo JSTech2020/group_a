f034664a4423437df07913acd76bde2a01284422
Murtaza Raza <murtazaraza.01@gmail.com>
2020-05-01 00:32:59 +0200

    add stories

--- visualization/client/src/components/TopicStories.vue
+++ visualization/client/src/components/TopicStories.vue
@@ -1,5 +1,18 @@
 <template>
-    <div class="container">
-    
-    </div>
+  <div class="container">
+    <ul class="nav nav-tabs">
+      <li class="nav-item">
+        <a class="nav-link active" href="#">Topic 1</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Topic 2</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Topic 3</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link disabled" href="#">Topic 4</a>
+      </li>
+    </ul>
+  </div>
 </template>
